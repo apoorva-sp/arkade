9e738921895b57407e68f5be02dc1370505e697e
Anirudh R Rao <anirudhrrao627@gmail.com>
2025-05-16 20:45:18 +0530

    random

--- today-i-learned/src/supabase.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from "./config";
-import { createClient } from "@supabase/supabase-js";
-console.log(CONFIG.API_KEY);
-const supabaseUrl = CONFIG.LINK;
-const supabaseKey = CONFIG.API_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
